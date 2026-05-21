76a1bb61fd0618ff643a158f5522f8e07ca543c8
Manish <nithish3046@gmail.com>
2026-05-21 23:04:45 +0530

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
-export default {
-   plugins: [react()],
-  base: "/Recipe-Finder-App/",
-};
+export default defineConfig({
+  plugins: [react()],
+})
